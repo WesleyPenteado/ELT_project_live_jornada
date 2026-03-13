e483a09abccd0afc0e59797958dcb81d7386d508
WesleyPenteado <wesleycosta.ne@gmail.com>
2026-03-12 19:48:17 -0700

    Atualizando view bronze_vendas e .gitignore

--- ecommerce/models/Bronze/bronze_vendas.sql
+++ ecommerce/models/Bronze/bronze_vendas.sql
@@ -1,9 +1,3 @@
--- ============================================
--- CAMADA BRONZE: Vendas (Dados Brutos)
--- ============================================
--- Conceito: Primeira camada da arquitetura Medalhão
--- Objetivo: Capturar dados exatamente como vêm da fonte
-
 SELECT
     id_venda,
     data_venda,
